682f77021d983a70419c1293a6dacbd2a350761e
nakul <nakullanjewar2001@gmail.com>
2022-02-04 14:57:22 +0530

    refactor ui

--- js/script.js
+++ js/script.js
@@ -5,23 +5,7 @@ function findBMI() {
   var BMI = (weight / (heightCm * heightCm)).toFixed(2);
   console.log(BMI);
 
-  if (!isNaN(BMI)) {
-    document.getElementById("txt-BMI").innerText = "BMI is " + BMI;
-    var remark = " ";
-    if (BMI < 18.5) {
-      remark = "Underweight";
-    } else if (BMI >= 18.5 && BMI < 25) {
-      remark = "Normal";
-    } else if (BMI >= 25 && BMI <= 29.9) {
-      remark = "Overweight";
-    } else if (BMI >= 30) {
-      remark = "Obese";
-    }
-    document.getElementById("txt-remark").innerHTML = "You are " + remark;
-  } else {
-    document.getElementById("txt-BMI").innerText =
-      "Hey, You need to Add height and weight first !👼👼";
-  }
+  setBMI(BMI);
 }
 
 function findBMIStd(){
@@ -33,29 +17,39 @@ function findBMIStd(){
     inches = 0;
   }
   var totalHeight = (feet+inches)/3.2808;
-
   var BMI = (wtPound / (totalHeight * totalHeight)).toFixed(2);
 
   console.log(BMI);
+  setBMI(BMI)
+  
+}
 
+function setBMI(BMI){
   if (!isNaN(BMI)) {
     document.getElementById("txt-BMI").innerText = "BMI is " + BMI;
     var remark = " ";
+    var cName= "";
     if (BMI < 18.5) {
       remark = "Underweight";
+      cName = "under-weight";
     } else if (BMI >= 18.5 && BMI < 25) {
       remark = "Normal";
+      cName = "normal";
     } else if (BMI >= 25 && BMI <= 29.9) {
       remark = "Overweight";
+      cName = "over-weight";
     } else if (BMI >= 30) {
       remark = "Obese";
+      cName = "obese";
     }
     document.getElementById("txt-remark").innerHTML = "You are " + remark;
+    document.getElementById("result").className= cName + " b-shadow";
   } else {
     document.getElementById("txt-BMI").innerText =
       "Hey, You need to Add height and weight first !👼👼";
   }
 }
+
 function showStandardUnit(stdCheckBox) {
   var std = document.getElementById("standard-unit");
   var metric = document.getElementById("metric-unit");
@@ -64,6 +58,7 @@ function showStandardUnit(stdCheckBox) {
 
   document.getElementById("txt-BMI").innerText ="";
   document.getElementById("txt-remark").innerHTML = "" ;
+  document.getElementById("result").className="";
 }
 
 function showMetricUnit(checkBox) {
@@ -74,4 +69,5 @@ function showMetricUnit(checkBox) {
 
   document.getElementById("txt-BMI").innerText ="";
   document.getElementById("txt-remark").innerHTML = "" ;
+  document.getElementById("result").className="";
 }
